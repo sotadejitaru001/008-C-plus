72a19045c1143502b09a2120d7e5b09898dd35e9
sotadejitaru001 <sota_music@icloud.com>
2024-01-24 17:14:57 +0900

    修正

--- Ex5_hint/Ex5_hint.cpp
+++ Ex5_hint/Ex5_hint.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solid
 {
 public:
-	virtual double GetVolume() = 0;
+	virtual double GetSize() = 0;
 	virtual string message() = 0;
 };
 
@@ -31,9 +31,9 @@ public:
 		this->height = height;
 		this->depth = depth;
 	}
-	double GetVolume() override
+	double GetSize() override
 	{
-		return width * height * depth;
+		return width + height + depth;
 	}
 	string message()
 	{
@@ -52,9 +52,9 @@ public:
 		this->radius = radius;
 		this->height = height;
 	}
-	double GetVolume()override
+	double GetSize()override
 	{
-		return radius * radius * M_PI * height;
+		return radius * 4+ height;
 	}
 	string message()
 	{
@@ -74,9 +74,9 @@ public:
 		this->radius = radius;
 		this->height = height;
 	}
-	double GetVolume()override
+	double GetSize()override
 	{
-		return radius * radius * M_PI * height * 1 / 3;
+		return radius * 4 + height;
 	}
 	string message()
 	{
@@ -93,9 +93,9 @@ public:
 	{
 		this->radius = radius;
 	}
-	double GetVolume()override
+	double GetSize()override
 	{
-		return 4.0 / 3 * radius * radius * radius * M_PI;
+		return radius * 6;
 	}
 	string message()
 	{
@@ -106,10 +106,10 @@ public:
 
 int main()
 {
-	Box box{ 9,2,10 };
-	Cylinder cylinder{ 2,5 };
-	Cone cone{ 3,3 };
-	Sphere sphere{ 3 };
+	Box box{ 20,50,110 };
+	Cylinder cylinder{ 50,50 };
+	Cone cone{ 30,30 };
+	Sphere sphere{ 20 };
 	int kuroneko[]={60,80,100,120,140,160,180,200};
 	int yuupack[] = { 60,80,100,120,140,160,170};
 	Solid* solids[] = { &box, &cylinder, &cone, &sphere };
@@ -118,27 +118,28 @@ int main()
 		for (int i = 0; i < _countof(kuroneko); i++)
 		{
 			
-			if (kuroneko[i] >= s->GetVolume()) {
+			if (kuroneko[i] >= s->GetSize()) {
 				if (i < _countof(kuroneko)) {
-					cout << s->message() << ":体積=" << s->GetVolume() << endl;
+					cout << s->message() << ":体積=" << s->GetSize() << endl;
 					cout << kuroneko[i] << "サイズ" << " " <<"黒猫" << endl;
 				}
 				break;
 			}
 		}
-		if (s->GetVolume() >kuroneko[7]) {
+		if (s->GetSize() >kuroneko[7]) {
+			cout << s->message() << ":体積=" << s->GetSize() << endl;
 			cout << s->message() << "は送れません" << " " << "黒猫" << endl;
 		}
 		for (int i = 0; i < _countof(yuupack); i++)
 		{
-			if (yuupack[i] >= s->GetVolume()) {
+			if (yuupack[i] >= s->GetSize()) {
 				if (i < _countof(yuupack)) {
 					cout << yuupack[i] << "サイズ" << " " << "ゆうパック" << "\n" << endl;
 				}
 				break;
 			}
 		}
-		if (s->GetVolume()>yuupack[6]) {
+		if (s->GetSize()>yuupack[6]) {
 			cout << s->message() << "は送れません" << " " << "ゆうパック" << "\n" << endl;
 		}
 	};
